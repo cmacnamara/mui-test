a5a7f13515cbed444f3d56f2871064a0a0dd320b
Chris MacNamara <cpmacnamara@gmail.com>
2023-09-18 12:24:45 -0400

    set up basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [display, setDisplay] = useState({
+    firstName: 'random',
+    lastName: 'name',
+    email: 'randomname@gmail.com',
+    location: 'Seattle',
+  })
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    location: '',
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <Box>
+        <Paper>
+          <form>
+            <TextField 
+              type='text'
+              name='firstName'
+              label='First Name'
+              value={formData.firstName}
+            />
+            <TextField 
+              type='text'
+              name='lastName'
+              label='Last Name'
+              value={formData.lastName}
+            />
+            <TextField 
+              type='text'
+              name='email'
+              label='Email'
+              value={formData.email}
+            />
+            <TextField 
+              type='text'
+              name='location'
+              label='Location'
+              value={formData.location}
+            />
+            <Button type="submit"> Submit </Button>
+          </form>
+        </Paper>
+      </Box>
+    </div>
   )
 }
 
