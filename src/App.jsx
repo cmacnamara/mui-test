c42f9d31f192be933fa073cb26c6a9af77a318c8
Chris MacNamara <cpmacnamara@gmail.com>
2023-09-18 12:41:52 -0400

    add display for form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
 
 import './App.css'
 
@@ -27,11 +30,46 @@ function App() {
     setFormData({ ...formData, [event.target.name]: event.target.value })
   }
 
+  const handleSubmit = event => {
+    event.preventDefault()
+    setDisplay(formData)
+  }
+
+  useEffect(() => {
+    console.log(display);
+  }, [display])
+
   return (
     <div className='App'>
       <Box>
         <Paper>
-          <form>
+          <List>
+            <ListItem>
+              <ListItemText
+                primary={`${display.firstName} ${display.lastName}`} 
+                secondary="Name"
+              />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <ListItemText
+                primary={`${display.email}`} 
+                secondary="Email"
+              />
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <ListItemText
+                primary={`${display.location}`} 
+                secondary="Location"
+              />
+            </ListItem>
+          </List>
+        </Paper>
+      </Box>
+      <Box>
+        <Paper>
+          <form onSubmit={handleSubmit}>
             <TextField 
               type='text'
               name='firstName'
